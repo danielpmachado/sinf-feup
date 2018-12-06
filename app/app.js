383a65b3eb31d204d38dc67370dac501af98f019
Nuno Pereira <up201506265@fe.up.pt>
2018-12-06 18:46:37 +0000

    Missing file

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ var hbs = require('express-handlebars');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var register = require('./routes/register');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/register', register);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
